8b7f7c483b7106cc10b9d7e058f1da4dd350c224
sam-pop <poplovitch.samuel@gmail.com>
2018-07-03 11:10:45 -0400

    model files added

--- server.js
+++ server.js
@@ -11,16 +11,20 @@ const PORT = process.env.PORT || 8080;
 app.use(express.static("public"));
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
 const exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 
 app.get('/', (req, res) => {
-    res.send('Welcome to TRUCKTRACK')
-})
+    res.send('Welcome to TRUCKTRACK');
+});
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
