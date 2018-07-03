e3911c3617fa966de08cd64cf5a38ece2a564758
sam-pop <poplovitch.samuel@gmail.com>
2018-07-03 11:59:02 -0400

    server updated with sequelize.sync

--- server.js
+++ server.js
@@ -9,19 +9,31 @@ const db = require('./models');
 const app = express();
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 app.use(express.static("public"));
-app.use(session({secret: "keyboard cat", resave: true, saveUninitialized: true}));
+app.use(session({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: true
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 const exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 
 app.get('/', (req, res) => {
     res.send('Welcome to TRUCKTRACK');
 });
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+db.sequelize.sync({
+    force: true
+}).then(function () {
+    app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+});
