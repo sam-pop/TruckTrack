07232d90fed595623a385a3b74b9e076a18a0f05
sam-pop <poplovitch.samuel@gmail.com>
2018-07-05 13:16:32 -0400

    html_routes update

--- routes/html_routes.js
+++ routes/html_routes.js
@@ -1,14 +1,22 @@
 var db = require("../models");
 
 module.exports = function (app) {
-    app
-        .get('/', function (req, res) {
-            res.send('<h1>hello truck world!</h1>');
-        });
+    app.get('/', function (req, res) {
+        res.send('<h1>hello truck world!</h1>');
+    });
+
+    // renders the signup page for users
     app.get("/signup", function (req, res) {
         res.render('signup');
     });
-    app.get("/signin", function (req, res) {
-        res.render('signin');
+
+    // renders the signup page for truck owners
+    app.get("/signupTruck", function (req, res) {
+        res.render('signupTruck');
+    });
+
+    // renders the login page
+    app.get("/login", function (req, res) {
+        res.render('login');
     });
 };
