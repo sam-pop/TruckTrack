93daf249c2ef7fc4d19c255f5d67715b4468f3ba
sam-pop <poplovitch.samuel@gmail.com>
2018-07-05 13:59:04 -0400

    routes update

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,6 +1,6 @@
 // Dependencies
 const User = require('../models/user');
-
+const passport = require("../config/passport");
 // Variables
 let db = require('../models');
 
@@ -12,36 +12,11 @@ module.exports = function (app) {
     });
 
     // POST signup page
-    // app.post('/signup', function (req, res) {
-    //     let user = {
-    //         email: "tesg@gmail.com",
-    //         password: 'passtest',
-    //         name: 'Test McTesty'
-    //     };
-    //     addUser(user);
-    //     res.end('test');
-    // });
-
     app.post('/signup', function (req, res) {
-        let user = {
-            email: req.body.email,
-            password: req.body.password,
-            name: req.body.name,
-        };
-        addUser(user);
-        // res.send('test');
-
+        db.User.create(req.body).then(function (dbUser) {
+            res.redirect(307, '/login');
+        }).catch(function (err) {
+            res.status(500).json(err);
+        });
     });
 };
-
-
-
-function addUser(user) {
-    db.User.create({
-        email: user.email,
-        password: user.password,
-        name: user.name
-    }).then(function (dbUser) {
-        console.log(dbUser);
-    });
-}
