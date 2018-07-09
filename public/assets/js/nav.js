8feaf31f0bb41e0aa38d1aa5f098087b438a6d7b
sam-pop <poplovitch.samuel@gmail.com>
2018-07-09 12:33:55 -0400

    dynamic nav bar

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -1,5 +1,5 @@
-$.get('/auth').then(function (data) {
-    if (data) {
+$.get('/auth').then(function (isAuth) {
+    if (isAuth) {
         $('#userLogin').html('<a class="nav-link" href="/profile">Profile</a>');
         $('#logout').html('<a class="nav-link" href="/logout">Logout</a>');
     }
