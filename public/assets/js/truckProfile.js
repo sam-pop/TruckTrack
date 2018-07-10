78573bc0a021ce97ab4b2c14840162d932ffe111
sam-pop <poplovitch.samuel@gmail.com>
2018-07-10 10:07:21 -0400

    location update update

--- public/assets/js/truckProfile.js
+++ public/assets/js/truckProfile.js
@@ -1,34 +1,38 @@
-$(function() {
-  getGeoLocaion();
-  $("#locationBtn").on("click", function(event) {
-    setTimeout(function() {
-      updateLocation(myLoc);
-    }, 5000);
-  });
+$(function () {
+    getGeoLocaion();
+    $("#locationBtn").on("click", function (event) {
+        if ($("#locationBtn").attr('class').indexOf('disabled') == -1) {
+            $('#locationBtn').addClass('disabled');
+            setTimeout(function () {
+                updateLocation(myLoc);
+                $('#locationBtn').removeClass('disabled');
+            }, 5000);
+        }
+    });
 });
 
 function getGeoLocaion() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(myLocation);
-  } else {
-    alert("Geolocation is not supported by this browser.");
-  }
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(myLocation);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
 }
 
 function myLocation(position) {
-  var myLat = position.coords.latitude;
-  var myLon = position.coords.longitude;
-  myLoc = {
-    lat: myLat,
-    lon: myLon
-  };
+    var myLat = position.coords.latitude;
+    var myLon = position.coords.longitude;
+    myLoc = {
+        lat: myLat,
+        lon: myLon
+    };
 }
 
 function updateLocation(latlon) {
-  $.post("/profile/truck/setLocation", latlon, function(data) {
-    if (data.lat && data.lon) alert("Location Updated!");
-    else {
-      alert("Location not found! try again...");
-    }
-  });
+    $.post("/profile/truck/setLocation", latlon, function (data) {
+        if (data.lat && data.lon) alert("Location Updated!");
+        else {
+            alert("Location not found! try again...");
+        }
+    });
 }
