fae2b8cf7d14cd15cce5c2c4131e4e953df2817d
Stanley Louis <stanlouis@msn.com>
2018-07-10 18:01:59 -0400

    add get request to user_update

--- public/assets/js/user_update.js
+++ public/assets/js/user_update.js
@@ -1,28 +1,43 @@
-var userUpdateForm = $("form.userUpdate")
-var nameInput = $("input#inputName");
-var emailInput = $("input#inputEmail");
+$(document).ready(function() {
+  var userUpdateForm = $("form.userUpdate")
+  var nameInput = $("input#inputName");
+  var emailInput = $("input#inputEmail");
 
-userUpdateForm.on("submit", function(event) {
-  event.preventDefault();
-  var userData = {
-    name: nameInput.val().trim(),
-    email: emailInput.val().trim()
-  };
-  console.log(userData);
+  getUserData();
+  userUpdateForm.on("submit", function(event) {
+    event.preventDefault();
+    var userData = {
+      name: nameInput.val().trim(),
+      email: emailInput.val().trim()
+    };
+    console.log(userData);
 
-  if (!userData.email || !userData.name) {
-    return;
+    if (!userData.email || !userData.name) {
+      return;
+    }
+    // getUserData();
+    updateUser(userData);
+    // emailInput.val("");
+    // nameInput.val("");
+  });
+
+  function getUserData() {
+    $(document).ready(function() {
+      $.get("/api/user_data").then(function(data) {
+        console.log('data from api', data);
+        nameInput.val(data.name);
+        emailInput.val(data.email);
+      });
+    });
   }
-  updateUser(userData);
-  emailInput.val("");
-  nameInput.val("");
-});
 
-function updateUser(userData) {
-  $.ajax({
-    method: "PUT",
-    url: "/userProfileUpdate",
-    data: userData
-  })
-}
+  function updateUser(userData) {
+    $.ajax({
+      method: "PUT",
+      url: "/userProfileUpdate",
+      data: userData
+    })
+  }
+})
+
 
