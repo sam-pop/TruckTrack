8f7bea8d61080a773f695460bb4dfa4f6d30bd3b
sam-pop <poplovitch.samuel@gmail.com>
2018-07-10 12:44:55 -0400

    bug fix

--- routes/html_routes.js
+++ routes/html_routes.js
@@ -1,40 +1,40 @@
 var isAuth_Destroy = require('../config/middleware/isAuth_Destroy');
 
-module.exports = function(app) {
+module.exports = function (app) {
 
   // homepage (index)
-  app.get('/', function(req, res) {
+  app.get('/', function (req, res) {
     res.render('index');
   });
 
   // renders the signup page for users
-  app.get('/signup', function(req, res) {
-    if(!req.user)
-    res.render('signup');
-    else res.redirect('/UserProfile')
+  app.get('/signup', function (req, res) {
+    if (!req.user)
+      res.render('signup');
+    else res.redirect('/UserProfile');
   });
 
-  app.get('/userProfile', function(req, res) {
-    res.render('userProfile')
-  })
+  app.get('/userProfile', function (req, res) {
+    res.render('userProfile');
+  });
+
   // renders the signup page for truck owners
-  app.get('/signupTruck', function(req, res) {
+  app.get('/signupTruck', function (req, res) {
     res.render('signupTruck');
   });
 
   // renders the login page
-  app.get('/login', function(req, res) {
+  app.get('/login', function (req, res) {
     if (!req.user)
       res.render('login');
     else res.redirect('/profile');
   });
 
-  app.get('/about', function(req, res) {
-    if (!req.user)
-      res.render('about');
+  app.get('/about', function (req, res) {
+    res.render('about');
   });
 
-  app.get('/userProfileUpdate', function(req, res) {
+  app.get('/userProfileUpdate', function (req, res) {
     res.render('userProfileUpdate');
   });
 
