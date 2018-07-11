d73e285c09795515827f0159ae9c6c410a2a47c0
sam-pop <poplovitch.samuel@gmail.com>
2018-07-10 21:20:27 -0400

    design upgrades

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -35,7 +35,7 @@ var truckCustomMaker = L.icon({
   iconUrl: "./assets/img/marker.png",
   iconSize: [34, 44],
   iconAnchor: [20, 36],
-  popupAnchor: [-3, -76],
+  popupAnchor: [-4, -36],
   shadowUrl: "",
   shadowSize: [68, 95],
   shadowAnchor: [22, 94]
@@ -66,11 +66,13 @@ $(function () {
     console.log(data);
 
     for (var i of data) {
+      //addes truck markers to map
       var latlon = [i.Location.lat, i.Location.lon];
       var truckMarker = L.marker(latlon, {
         icon: truckCustomMaker
       });
-      truckMarker.bindPopup(i.truckName);
+      var popup = "<div class='text-center'><span style=\"font-size:1.2em;color:#2c73d2;\"><b>" + i.truckName + "</b></span><hr><a href=\"/profile/truck/" + i.id + "\"><img width=\"100%\" src=\"" + i.pictureURL + "\"></a><hr><i>" + i.category + "</i></div>";
+      truckMarker.bindPopup(popup);
       truckMarker.addTo(mymap);
       //addes truck to the truck list
       if (i.truckName) {
