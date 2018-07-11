8447a4127ba79b9fe597349d0feab97dc61a0a17
sam-pop <poplovitch.samuel@gmail.com>
2018-07-10 20:20:49 -0400

    design updates and tweaks

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -1,8 +1,8 @@
 $('#separator').hide();
 $.get("/auth").then(function (isAuth) {
   if (isAuth) {
-    $("#userLogin").html('<a class="nav-link" href="/profile">Profile</a>');
+    $("#userLogin").html("<a class='nav-link' href='/profile'><i class='material-icons' style='font-size:0.8em'>lock_open</i>&nbsp;Profile</a>");
     $('#separator').show();
-    $("#logout").html('<a class="nav-link" href="/logout">Logout</a>');
+    $("#logout").html('<a class="nav-link" href="/logout" style="font-weight:bold;">Logout</a>');
   }
 });
