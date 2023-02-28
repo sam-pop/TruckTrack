4ea78c8a9a481cd2087018cdf59277ea36827e03
Samuel Poplovitch <poplovitch.samuel@gmail.com>
2023-02-27 20:41:38 -0500

    update 2023

--- models/user.js
+++ models/user.js
@@ -1,54 +1,58 @@
 var bcrypt = require("bcrypt-nodejs");
-var validator = require('validator');
+var validator = require("validator");
 
 module.exports = function (sequelize, DataTypes) {
-    // creates our User table
-    var User = sequelize.define("User", {
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true
-            // we use a hook to handle the email validation (using validator package)
-            // instead of the built-in isEmail validation method (gmail addresses false negative)
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        isTruckOwner: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false,
-            allowNull: false
-        }
-    });
+  // creates our User table
+  var User = sequelize.define("User", {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      // we use a hook to handle the email validation (using validator package)
+      // instead of the built-in isEmail validation method (gmail addresses false negative)
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    isTruckOwner: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: false,
+    },
+  });
 
-    // isEmail validation for the user email column 
-    User.hook('beforeValidate', function (user) {
-        // workaround for false error response for gmail addresses
-        if (user.email.indexOf('@gmail.com') != -1) {
-            return sequelize.Promise.resolve(user);
-        }
-        // isEmail validation
-        if (validator.isEmail(user.email)) {
-            return sequelize.Promise.resolve(user);
-        } else {
-            return sequelize.Promise.reject('Validation Error: invalid email');
-        }
-    });
+  // isEmail validation for the user email column
+  User.beforeValidate(function (user) {
+    // workaround for false error response for gmail addresses
+    if (user.email.indexOf("@gmail.com") != -1) {
+      return sequelize.Promise.resolve(user);
+    }
+    // isEmail validation
+    if (validator.isEmail(user.email)) {
+      return sequelize.Promise.resolve(user);
+    } else {
+      return sequelize.Promise.reject("Validation Error: invalid email");
+    }
+  });
 
-    // User protoype method to check if our user password is valid 
-    User.prototype.validPassword = function (password) {
-        return bcrypt.compareSync(password, this.password);
-    };
+  // User protoype method to check if our user password is valid
+  User.prototype.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+  };
 
-    // pre table creation hook to hash the user password 
-    User.hook("beforeCreate", function (user) {
-        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(12), null);
-    });
+  // pre table creation hook to hash the user password
+  User.beforeCreate(function (user) {
+    user.password = bcrypt.hashSync(
+      user.password,
+      bcrypt.genSaltSync(12),
+      null
+    );
+  });
 
-    return User;
+  return User;
 };
--- server.js
+++ server.js
@@ -3,32 +3,34 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var session = require("express-session");
 var passport = require("./config/passport");
-var sequelize_fixtures = require('sequelize-fixtures');
-var models = require('./models');
-
-
+var sequelize_fixtures = require("sequelize-fixtures");
+var models = require("./models");
 
 // Variables
 var PORT = process.env.PORT || 8080;
 
-var db = require('./models');
+var db = require("./models");
 var app = express(); // express app init
 
 // sets up the express app to handle data parsing
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 
 // static serve the public folder to our express app
 app.use(express.static("public"));
 
 // init and config our express session
-app.use(session({
+app.use(
+  session({
     secret: "big balagan",
     resave: true,
-    saveUninitialized: true
-}));
+    saveUninitialized: true,
+  })
+);
 
 // passport init
 app.use(passport.initialize());
@@ -36,22 +38,31 @@ app.use(passport.session());
 
 // handlebars view engine init
 var exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({
-    defaultLayout: "main"
-}));
+app.engine(
+  "handlebars",
+  exphbs.engine({
+    defaultLayout: "main",
+  })
+);
 app.set("view engine", "handlebars");
 
 // router
-require('./controllers/user_controller')(app);
-require('./controllers/truck_controller')(app);
-require('./routes/html_routes')(app);
-
+require("./controllers/user_controller")(app);
+require("./controllers/truck_controller")(app);
+require("./routes/html_routes")(app);
 
 // sequelize db sync + app listen
 
-db.sequelize.sync({
-    force: true
-}).then(function () {
-    sequelize_fixtures.loadFile('./db/test_data.json', models);
-    app.listen(PORT, () => console.log(` | Server listening on port ${PORT} |` + '\n ---------------------------------'));
-});
+db.sequelize
+  .sync({
+    force: true,
+  })
+  .then(function () {
+    sequelize_fixtures.loadFile("./db/test_data.json", models);
+    app.listen(PORT, () =>
+      console.log(
+        ` | Server listening on port ${PORT} |` +
+          "\n ---------------------------------"
+      )
+    );
+  });
